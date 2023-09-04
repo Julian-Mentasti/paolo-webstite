e6e7467838a8914a0758f3a2e72dc50ccf468aab
paolomentasti <81896149+paolomentasti@users.noreply.github.com>
2023-09-04 18:11:24 -0400

    Heaven page was added. Nav bar updated. About and CV updated

--- analytics.js
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-90GT35QDBZ"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-90GT35QDBZ');
-</script>
